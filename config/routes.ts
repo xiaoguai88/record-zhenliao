a54bc8db9322201ebe275bfe5a5df2b0af1abf8d
wb-wwj777144 <wb-wwj777144@alibaba-inc.com>
2021-12-10 16:47:54 +0800

    first

---
+++ config/routes.ts
@@ -0,0 +1,64 @@
+﻿export default [
+  {
+    path: '/user',
+    layout: false,
+    routes: [
+      {
+        path: '/user',
+        routes: [
+          {
+            name: 'login',
+            path: '/user/login',
+            component: './user/Login',
+          },
+        ],
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    path: '/welcome',
+    name: 'welcome',
+    icon: 'smile',
+    component: './Welcome',
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    icon: 'crown',
+    access: 'canAdmin',
+    component: './Admin',
+    routes: [
+      {
+        path: '/admin/sub-page',
+        name: 'sub-page',
+        icon: 'smile',
+        component: './Welcome',
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    name: 'list.table-list',
+    icon: 'table',
+    path: '/list',
+    component: './TableList',
+  },
+  {
+    name: 'my-record',
+    // icon: 'checkbox',
+    path: '/checkbox',
+    component: './MyCheckbox',
+  },
+  {
+    path: '/',
+    redirect: '/welcome',
+  },
+  {
+    component: './404',
+  },
+];
---
+++ src/pages/MyCheckbox/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button } from 'antd';
+
+const MyCheckbox = (props) => {
+  return (
+    <div>
+      <Button>记录</Button>
+      MyCheckbox
+    </div>
+  );
+};
+
+MyCheckbox.propTypes = {};
+
+export default MyCheckbox;
