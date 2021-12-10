0fd6fb2d79a0fc7f05cee29de974bb562b49ed6e
wb-wwj777144 <wb-wwj777144@alibaba-inc.com>
2021-12-10 17:38:22 +0800

    fix:up

--- src/pages/MyCheckbox/index.tsx
+++ src/pages/MyCheckbox/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button } from 'antd';
+import { Button, Modal } from 'antd';
 
 const MyCheckbox = (props) => {
   return (
     <div>
-      <Button>记录</Button>
-      MyCheckbox
+      <Button>记录</Button>0000
+      <Modal />
     </div>
   );
 };
